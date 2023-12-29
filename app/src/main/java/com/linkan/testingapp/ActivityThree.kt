72fe9264d9bdea8ed563fbf13f56ade97df10518
Linkan Chauhan <linkanchauhan610@gmail.com>
2023-12-29 20:53:52 +0530

    Activity navigation with flag no history

---
+++ app/src/main/java/com/linkan/testingapp/ActivityThree.kt
@@ -0,0 +1,12 @@
+package com.linkan.testingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.atilsamancioglu.artbookhilttesting.R
+
+class ActivityThree : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_three)
+    }
+}
